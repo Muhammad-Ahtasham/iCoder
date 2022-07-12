0aa654b6a05c4f00cbf024a39f42bc8d7e988130
ATIII <ahtashamahsan988@gmail.com>
2022-07-12 18:43:54 +0500

    added comment section in blogposts for any user to enter a comment

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from blog import views
 from django.urls import path
 
 urlpatterns = [
+     # API to post comments 
+     path('postComment', views.postComment, name='postComment'),
+     
+     
      path('', views.blogHome, name='blogHome'),
-     path('<str:slug>', views.blogPost, name='blogPost')
+     path('<str:slug>', views.blogPost, name='blogPost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
-from blog.models import Post
+from django.shortcuts import redirect, render, HttpResponse
+from blog.models import Post, BlogComment
+
+from django.contrib import messages
 # Create your views here.
 def blogHome(request):
     allPosts = Post.objects.all()
@@ -10,7 +11,21 @@ def blogHome(request):
     return render(request, 'blog/blogHome.html', context)
 def blogPost(request, slug):
     post = Post.objects.filter(slug= slug).first()
+    comments = BlogComment.objects.filter(post = post)
     context = {
-        'post': post
+        'post': post, 
+        'comments': comments
     }
     return render(request, 'blog/blogPost.html', context)
+
+def postComment(request):
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        user = request.user
+        postSno = request.POST.get('postSno')
+        post = Post.objects.get(sno = postSno)
+        # parent = request.POST.get('')
+        commentS = BlogComment(comment= comment, user = user, post = post)
+        commentS.save()
+        messages.success(request, 'Your Comment has been posted')
+    return redirect(f'/blog/{post.slug}')
